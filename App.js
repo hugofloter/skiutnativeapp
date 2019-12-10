29f4760808163c525e5b16608b9d35640323d567
quentin <quentinrichard97@gmail.com>
2019-12-10 17:25:12 +0100

    add redux to App

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { Provider } from "react-redux";
+import { createStore } from "./api/state";
 
 import AppNavigator from './navigation/AppNavigator';
+
+const store = createStore({});
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -21,8 +25,10 @@ export default function App(props) {
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
+        <Provider store={store}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </Provider>
       </View>
     );
   }
