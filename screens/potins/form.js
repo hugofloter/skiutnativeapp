bd032e29f3948888dab4004d8d2884bcedd77ad8
hugofloter <hugo.paigneau@etu.utc.fr>
2019-12-23 19:19:10 +0100

    [Add] header on form

--- screens/potins/form.js
+++ screens/potins/form.js
@@ -13,35 +13,43 @@ import Sizes from "../../constants/Sizes";
 const Form = () => {
    return (
      <View style = { styles.container }>
-       <Text style = { styles.text }>
-         Ceci est le formulaire
-       </Text>
+       <View style={ styles.headerContainer }>
+         <Text style={ styles.annulationTitle }>Annuler</Text>
+         <Text style={ styles.title }>Créer</Text>
+       </View>
+       <View style={ styles.contentContainer }>
+           <Text>
+             Ceci est le formulaire
+           </Text>
+       </View>
      </View>
    )
 }
 
 
-Form.navigationOptions = {
-    title: 'Home',
-    headerStyle: {
-      backgroundColor: '#f4511e',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-};
-
- const styles = StyleSheet.create({
-   container: {
-        flex: 1,
-        backgroundColor: Colors.defaultBackgroud,
-        padding: 5,
-   },
-   text: {
-     color: 'red',
-   }
- })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.defaultBackgroud,
+  },
+  headerContainer: {
+    display: "flex",
+    justifyContent: 'center',
+    padding: 5,
+    height: 50,
+    backgroundColor: "#fff",
+  },
+  title: {
+    textAlign: 'center'
+  },
+  annulationTitle: {
+    textAlign: "left"
+  },
+  contentContainer: {
+    padding: 5,
+    paddingTop: 30,
+  },
+})
 
 
 export default Form;
