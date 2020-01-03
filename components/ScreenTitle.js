7d88a2401f0d78047a506b423f6e1dffc5d68ad1
hugofloter <hugo.paigneau@etu.utc.fr>
2020-01-03 10:27:16 +0100

    Header improvment

--- components/ScreenTitle.js
+++ components/ScreenTitle.js
@@ -2,15 +2,21 @@ import React from "react";
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet, Image
 } from "react-native";
-
+import { Header } from "react-navigation"
 import Colors from "../constants/Colors";
 import Sizes from "../constants/Sizes";
+
+const TITLE_OFFSET = Platform.OS === 'ios' ? 70 : 56;
 
 const ScreenTitle = ({ title, children }) => {
   return (
     <View style={ styles.container }>
+        <Image
+            style={styles.headerLogo}
+            source={require("../assets/images/header-logo.png")}
+        />
       <Text style = { styles.title }>{ title }</Text>
       { children }
     </View>
@@ -22,13 +28,22 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'stretch',
-    marginTop: 5
+    alignItems: 'center',
+    height: Header.HEIGHT
   },
   title: {
     color: Colors.darkGrey,
     fontSize: Sizes.h1,
     fontWeight: 'bold',
+    textAlign: 'center',
+    position: 'absolute',
+    left: TITLE_OFFSET,
+    right: TITLE_OFFSET
+
+  },
+  headerLogo: {
+    width: Header.HEIGHT + 10,
+    height: Header.HEIGHT - 10
   }
 })
 
